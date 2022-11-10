6eb9a46da74d99693765ac4405c57d8736783b88
hihello119 <obsessed119@naver.com>
2022-11-10 18:43:26 +0900

    [Chapter 2] 정상적인 JSX

--- chapter2/src/App.js
+++ chapter2/src/App.js
@@ -5,10 +5,11 @@ function App() {
   const name = '리액트';
   return (
     <>
-      <div className=“react“>{name}</div>
-      <input>
+      <div className='react'>{name}</div>
+      <input></input>
     </>
   );
 }
+
 
 export default App;
