c61250bd817aff6c505a2a1cc0d07e8d127a660a
hihello119 <obsessed119@naver.com>
2022-11-10 18:44:10 +0900

    [Chapter 2] self-closing 태그

--- chapter2/src/App.js
+++ chapter2/src/App.js
@@ -5,11 +5,10 @@ function App() {
   const name = '리액트';
   return (
     <>
-      <div className='react'>{name}</div>
-      <input></input>
+      <div className="react">{name}</div>
+      <input />
     </>
   );
 }
-
 
 export default App;
