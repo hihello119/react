9e44d0352a61f6a05f1ff1d2814b690c08079f22
hihello119 <obsessed119@naver.com>
2022-11-10 18:32:00 +0900

    [Chapter 2] Fragment

--- chapter2/src/App.js
+++ chapter2/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment } from 'react';
+ 
 function App() {
   return (
-    <div>
+    <Fragment>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
-    </div>
+    </Fragment>
   );
 }
-
+ 
 export default App;
