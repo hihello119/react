b8a9922c202a029ead68ccde919c2a4712f1c3a2
hihello119 <obsessed119@naver.com>
2022-11-11 11:13:37 +0900

    [Chapter 9] sass 설정

--- chapter9/config/webpack.config.js
+++ chapter9/config/webpack.config.js
@@ -514,11 +514,17 @@ module.exports = function (webpackEnv) {
               }).concat({
                 loader: require.resolve("sass-loader"),
                 options: {
-                  includePaths: [paths.appSrc + "/styles"],
-                  sourceMap: isEnvProduction && shouldUseSourceMap,
-                  data: `@import 'utils';`,
+                  // prependData: `@import 'utils';`,
+                  sassOptions: {
+                    includePaths: [paths.appSrc + "/styles"],
+                    sourceMap: isEnvProduction && shouldUseSourceMap,
+                  },
                 },
               }),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
             },
             // Adds support for CSS Modules, but using SASS
