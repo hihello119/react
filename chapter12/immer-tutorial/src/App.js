2393d384e82abffc4b6c60caf557f6399edee341
hihello119 <obsessed119@naver.com>
2022-11-11 21:01:35 +0900

    [Chapter 12] immer를 사용하지 않고 불변성 유지

--- chapter12/immer-tutorial/src/App.js
+++ chapter12/immer-tutorial/src/App.js
@@ -3,7 +3,7 @@ import React, { useRef, useCallback, useState } from 'react';
 
 const App = () => {
   const nextId = useRef(1);
-  const [form, setForm] = useState({ name: ", username: " });
+  const [form, setForm] = useState({ name: '', username: '' });
   const [data, setData] = useState({
     array: [],
     uselessValue: null
