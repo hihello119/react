93e5b7dfd4ecdc4a496ba8abc819a5e628a69d86
hihello119 <obsessed119@naver.com>
2022-11-11 22:19:47 +0900

    [Chapter 13] withRouter

--- chapter13/router-tutorial/src/Profile.js
+++ chapter13/router-tutorial/src/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import WithRouterSample from './WithRouterSample';
 
 
 const data = {
@@ -15,21 +17,19 @@ const data = {
 
 
 const Profile = ({ match }) => {
-  const { username } = match.params;
-  const profile = data[username];
-  if (!profile) {
-    return <div>존재하지 않는 사용자입니다.</div>;
-  }
-  return (
-    <div>
-      <h3>
-        {username}({profile.name})
-      </h3>
-      <p>{profile.description}</p>
-    </div>
-  );
-};
+    const { username } = match.params;
+    const profile = data[username];
+    if (!profile) {
+      return <div>존재하지 않는 사용자입니다.</div>;
+    }
+    return (
+      <div>
+        (…)
+        <WithRouterSample />
+      </div>
+    );
+  };
 
 
 
-export default Profile;
+  export default withRouter(Profile);
