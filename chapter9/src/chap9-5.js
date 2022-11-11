080563ed69c39c2ff6c8f73c68dad366b3993c08
hihello119 <obsessed119@naver.com>
2022-11-11 11:35:46 +0900

    [Chapter 9] 스타일링 엘리먼트 예제

---
+++ chapter9/src/chap9-5.js
@@ -0,0 +1,8 @@
+const Sample = ({ className }) => {
+    return <div className={className}>Sample</div>;
+  };
+  
+  
+  const StyledSample = styled(Sample)</span>
+    <span class="co33">font-size</span><span class="co34">:</span> <span class="co32">2rem</span><span class="co36">;</span>
+  <span class="co31">;
