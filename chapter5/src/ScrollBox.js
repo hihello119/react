23fee45a694bb3d56e846beb8d70311638f99429
hihello119 <obsessed119@naver.com>
2022-11-11 10:19:55 +0900

    [Chapter 5] 스크롤 박스 컴포넌트 렌더링

---
+++ chapter5/src/ScrollBox.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class ScrollBox extends Component {
+  render() {
+    const style = {
+      border: "1px solid black",
+      height: "300px",
+      width: "300px",
+      overflow: "auto",
+      position: "relative",
+      //   width: '100%',
+      //   height: '650px',
+      //   background: 'linear-gradient(white, black)'
+    };
+
+    {
+      /* <span class="co50">const</span><span class="cd2"> innerStyle </span><span class="co40">=</span><span class="cd2"> {</span> */
+    }
+
+    return (
+      <div
+        style={style}
+        ref={(ref) => {
+          this.box = ref;
+        }}
+      >
+        <div style={style} />
+      </div>
+    );
+  }
+}
+
+export default ScrollBox;
