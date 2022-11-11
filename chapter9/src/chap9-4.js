0b0f6fa21555ab61b336481fa6e9ed8fe0582493
hihello119 <obsessed119@naver.com>
2022-11-11 11:35:21 +0900

    [Chapter 9] 스타일링 엘리먼트 예제

---
+++ chapter9/src/chap9-4.js
@@ -0,0 +1,8 @@
+// 태그의 타입을 styled 함수의 인자로 전달
+const MyInput = styled(‘input‘)</span>
+  <span class="co33">background</span><span class="co34">:</span> <span class="co32">gray</span><span class="co36">;</span>
+<span class="co31">
+// 아예 컴포넌트 형식의 값을 넣어 줌
+const StyledLink = styled(Link)</span>
+  <span class="co33">color</span><span class="co34">:</span> <span class="co32">blue</span><span class="co36">;</span>
+<span class="co31"></span>
